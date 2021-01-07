560a8aafc6fe82dd74494d514032a17dc1c992e9
ahmed <ahmedelcoach30@gmail.com>
2021-01-08 01:18:01 +0200

    make one to many relation between person and register table

--- app/Models/Personaldatastudent.php
+++ app/Models/Personaldatastudent.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use App\Models\Registration;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -10,4 +11,10 @@ class Personaldatastudent extends Model
     use HasFactory;
     protected $table='personaldatastudents';
     protected $guarded=[];
+    protected $primaryKey  = 'idS';
+    public function register()
+    {
+
+        return $this->hasMany(Registration::class,'idSF','idS');
+    }
 }
---
+++ database/migrations/2021_01_07_155420_add_new8_to_registrations_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNew8ToRegistrationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('registrations', function (Blueprint $table) {
+            $table->bigIncrements('idRegistration');
+            $table->string('arabicTitle', 100)->nullable();
+            $table->string('englishTitle', 100)->nullable();
+            $table->string('requiredCourses', 100)->nullable();
+            $table->double('toeflGrade')->nullable();
+            $table->date('departmentApprovalDateRegistration')->nullable();
+            $table->date('facultyApprovalDateRegistration')->nullable();
+            $table->date('universitydepartmentApprovalDateRegistration')->nullable();
+            $table->date('committeeytApprovalDateRegistration')->nullable();
+            $table->date('formDate')->nullable();
+            $table->string('currentState', 50)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('registrations', function (Blueprint $table) {
+            //
+        });
+    }
+}
