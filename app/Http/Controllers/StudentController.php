4d17ffb41d21543445937e4d9c2b3a6964ae046d
ahmed hassan <ahmedelcoach30@gmail.com>
2021-01-10 15:49:42 +0200

    delete the migration

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -22,18 +22,9 @@ class StudentController extends Controller
         $student=new Personaldatastudent();
         $student->englishName=$request->englishName;
         $student->arabicName=$request->arabicName;
-        $student->birthdateSource=$request->birthdateSource;
-        $student->birthdate=$request->birthdate;
-        $student->jobArabic=$request->jobArabic;
-        $student->jobEnglish=$request->jobEnglish;
-        $student->jobAdd=$request->jobAdd;
-        $student->Add=$request->Add;
-        $student->religion=$request->religion;
-        $student->nationality=$request->nationality;
+
         $student->email=$request->email;
-        $student->mobile=$request->mobile;
-        $student->nationalityId=$request->nationalityId;
-        $student->gender=$request->gender;
+
         $student->save();
         $user = DB::table('personaldatastudents')->orderBy('idS', 'desc')->first();
         $user_id=$user->idS;
