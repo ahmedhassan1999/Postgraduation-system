fd6e716184dead333fbe1e93e5fd249b3c422182
eslam sobhy ahmed abdallah <eslamsobhy206@gmail.com>
2021-01-13 01:48:26 +0200

    migrated Excuses table

---
+++ database/migrations/2021_01_12_233254_create_excuses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExcusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('excuses', function (Blueprint $table) {
+            $table->id('idExcuse');
+            $table->date('excuseDate')->nullable();
+            $table->date('cancelDate')->nullable();
+            $table->string('submittedDocURL', 400)->nullable();
+            $table->string('extendedPeriodDocURL', 400)->nullable();
+            $table->integer('numberMonthExtendedPeriod')->unsigned()->nullable();
+            $table->string('content', 300)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('excuses');
+    }
+}
