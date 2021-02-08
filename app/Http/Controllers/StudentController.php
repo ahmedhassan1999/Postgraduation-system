e15dd15b0e2c17026c62937d579811b7cefb041a
ahmed hassan <ahmedelcoach30@gmail.com>
2021-02-08 15:23:08 +0200

    send email by type of study

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Personaldatastudent;
 
 use App\Mail\StudentMail;
@@ -19,32 +20,37 @@ class StudentController extends Controller
     public function addStudentData(Request $request)
     {
 
-        $student=new Personaldatastudent();
-        $student->englishName=$request->englishName;
-        $student->arabicName=$request->arabicName;
+        $student = new Personaldatastudent();
+        $student->englishName = $request->englishName;
+        $student->arabicName = $request->arabicName;
+        $student->study_type = $request->study_type;
 
-        $student->email=$request->email;
+        $student->email = $request->email;
 
         $student->save();
         $user = DB::table('personaldatastudents')->orderBy('idS', 'desc')->first();
-        $user_id=$user->idS;
-        $user_name=$user->englishName;
+        $user_id = $user->idS;
+        $user_name = $user->englishName;
+        $user_type = $user->study_type;
         //return dd($user_name);
+        $name = " ";
+        if ($user_type == "دكتوراه الفلسفة في العلوم")
+            $name = "https://forms.office.com/Pages/ResponsePage.aspx?id=ZVH5axNBiEGbe8tsDBmKW-kPX0-Y8GNGh3ca7Z_4igRUMURFUTNSTk5UVlJPOEg5MDNIMEhVU0o1Wi4u";
+        else if ($user_type == "دبلومة الدراسات العليا")
+            $name = "https://forms.office.com/Pages/ResponsePage.aspx?id=ZVH5axNBiEGbe8tsDBmKW-kPX0-Y8GNGh3ca7Z_4igRUMDhCQ0ZOWk5CNjNEMEFQNDg2WEo0WjZEQi4u";
+        else if ($user_type == "لماجستير في العلوم")
+            $name = "https://forms.office.com/Pages/ResponsePage.aspx?id=ZVH5axNBiEGbe8tsDBmKW-kPX0-Y8GNGh3ca7Z_4igRUNDNQT0tHNUVFNlJLVDJHMVU4NFo5SjFERi4u";
+        else if ($user_type == "تمهيدي الماجستير")
+            $name = "https://forms.office.com/Pages/ResponsePage.aspx?id=ZVH5axNBiEGbe8tsDBmKW-kPX0-Y8GNGh3ca7Z_4igRUMDhCQ0ZOWk5CNjNEMEFQNDg2WEo0WjZEQi4u";
 
+        // $name = "https://forms.gle/xQgRdk2Ra89d6foPA";
 
-       $name="https://forms.gle/xQgRdk2Ra89d6foPA";
-
-        Mail::to($user->email)->send(new StudentMail( $user_id,$user_name,$name));
-        echo "send to ".$user->englishName . " ". "Done!";
+        Mail::to($user->email)->send(new StudentMail($user_id, $user_name, $name));
+        echo "send to " . $user->englishName . " " . "Done!";
 
 
         return response()->json([
             $student
         ], 201);
-
-
-
-
-
     }
 }
--- resources/views/emails/start.blade.php
+++ resources/views/emails/start.blade.php
@@ -6,7 +6,7 @@
 <body>
 
 
-<p>welcome {{$user_name}} we ask you to fill the form and send to us </p>
+<p> welcome  {{$user_name}} we ask you to fill the form and send to us </p>
 <p> the link of the form is {{$name}}   </p>
 <p> and you must use id {{$user_id}}  </p>
 
