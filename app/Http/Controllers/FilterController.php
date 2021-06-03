20563bf8bc9a84f5ea1f589e46e50eafc73459e6
eslam sobhy ahmed abdallah <eslamsobhy206@gmail.com>
2021-06-03 07:20:07 +0200

    added finished students filter

--- app/Http/Controllers/FilterController.php
+++ app/Http/Controllers/FilterController.php
@@ -35,15 +35,29 @@ class FilterController extends Controller
     public function finishedStudents(Request $request){
         $study = $request->study_type;
         if($study == "تمهيدي الماجستير"){
-            $studyId = StudyType::where('type', "تمهيدي الماجستير")->get();
-            $regist = Registration::where('currentState', 'finished')->where('idStudyTypeF')
+            $studies= StudyType::select('idStudyType')->distinct()->where('type', "تمهيدي الماجستير")->get()->pluck('idStudyType');
         }else if($study == "الماجستير في العلوم"){
-
+            $studies= StudyType::select('idStudyType')->distinct()->where('type', "الماجستير في العلوم")->get()->pluck('idStudyType');
         }else if($study == "دبلومة الدراسات العليا"){
-
+            $studies= StudyType::select('idStudyType')->distinct()->where('type', "دبلومة الدراسات العليا")->get()->pluck('idStudyType');
         }else if($study == "دكتوراه الفلسفة في العلوم"){
-
+            $studies= StudyType::select('idStudyType')->distinct()->where('type', "دكتوراه الفلسفة في العلوم")->get()->pluck('idStudyType');
         }
+        $regist = Registration::select('idSF')->distinct()->whereIn('idStudyTypeF',  $studies)->where('currentState','finished')->get()->pluck('idSF');
+        $students = Personaldatastudent::whereIn('idS', $regist)->get();
+
+        if($request->filled('from') && $request->filled('to')){
+            $from = date($request->from);
+            $to = date($request->to);
+            $students = $students->whereBetween('created_at', [$from, $to]);
+        }elseif($request->filled('from')){
+            $from = date($request->from);
+            $students = $students->where('created_at', ">=", $from);
+        }
+        if($request->filled('gender')){
+            $students = $students->where('gender', $request->gender);
+        }
+        return response()->json($students, 201);
     }
     
 }
